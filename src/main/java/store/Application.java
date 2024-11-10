007cbefa494512d36b51ec919d755629d6ead2da
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2024-11-10 22:20:21 +0900

    feat(Application) : 편의점 프로세스 구현
    절차적으로 메소드 실행 및 순서에 따른 분리

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,8 +1,39 @@
 package store;
 
+import java.util.List;
+import java.util.Map;
+import store.view.InputView;
+import store.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
 
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
         FileLoader fileLoader = new FileLoader();
+        PointOfSales pointOfSales = new PointOfSales();
+
+        List<String> products = fileLoader.loadProductsFile();
+        List<String> promotionsData = fileLoader.loadPromotionsFile();
+        Inventory inventory = new Inventory(products, promotionsData);
+        MembershipDiscount membershipDiscount = new MembershipDiscount();
+
+        boolean continueShopping = true;
+        while (continueShopping) {
+            OutputView.displayProducts(inventory);
+
+            Map<String, Integer> product = inputView.purchaseProduct();
+            Map<String, Map<String, Integer>> purchaseResults = pointOfSales.processPurchase(product,
+                    inputView);
+
+            System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
+            boolean applyMembershipDiscount = InputView.confirmUserInput();
+
+            OutputView.displayFinalReceipt(inventory, purchaseResults, membershipDiscount, applyMembershipDiscount);
+
+            if (!InputView.confirmUserInput()) {
+                continueShopping = false;
+            }
+        }
     }
 }
