1f6c28a724e50ba204886ddfd55356e6d8b26f57
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2024-11-10 21:34:45 +0900

    feat(OutputView) : OutView 클래스 추가
    ConsoleWriter 클래스 주입하여 기능 추가

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,4 +1,21 @@
-package store.view.input;
+package store.view;
+
+import store.view.output.ConsoleWriter;
+import store.view.output.Writer;
 
 public class OutputView {
+
+    private static final Writer DEFAULT_WRITER = new ConsoleWriter();
+
+    private final Writer writer;
+
+    public OutputView() {
+        this(DEFAULT_WRITER);
+    }
+    
+    public OutputView(Writer writer) {
+        this.writer = writer;
+    }
+
+
 }
