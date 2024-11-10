5e5286190e1ae80f95591e46e893762689668957
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2024-11-10 21:43:08 +0900

    feat(PromotionType) : PromotionType Enum 클래스 추가

--- src/main/java/store/PromotionType.java
+++ src/main/java/store/PromotionType.java
@@ -1,4 +1,20 @@
 package store;
 
 public enum PromotionType {
+
+    NONE(""),
+    MD_RECOMMENDATION("MD추천상품"),
+    BUY_2_GET_1("탄산2+1"),
+    BUY_1_GET_1("1_1"),
+    FLASH_SALE("반짝할인");
+
+    private final String displayName;
+
+    PromotionType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
