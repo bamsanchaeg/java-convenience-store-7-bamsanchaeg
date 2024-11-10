033383be74a7b539b7e2bfeb2dd8b36e91d08d0b
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2024-11-10 21:33:41 +0900

    feat(InputView) : InputView 클래스 추가 및 ConsoleReader 주입

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,4 +1,19 @@
 package store.view;
 
+import store.view.input.ConsoleReader;
+import store.view.input.Reader;
+
 public class InputView {
+
+    private static final Reader DEFAULT_READER = new ConsoleReader();
+
+    private final Reader reader;
+
+    public InputView(Reader reader) {
+        this.reader = reader;
+    }
+
+
+
+
 }
