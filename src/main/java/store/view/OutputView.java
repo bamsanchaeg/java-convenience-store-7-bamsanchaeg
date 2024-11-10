952feb530337f43d9ef5bab2f3bf7f5f3a9df9c2
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2024-11-10 22:14:47 +0900

    feat(OutputView) : 상품 재고 내역 출력 기능 및 영수증 출력 기능 구현

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,5 +1,12 @@
 package store.view;
 
+import java.util.List;
+import java.util.Map;
+import store.Inventory;
+import store.MembershipDiscount;
+import store.Product;
+import store.PromotionType;
+import store.Receipt;
 import store.view.output.ConsoleWriter;
 import store.view.output.Writer;
 
@@ -15,6 +22,59 @@ public class OutputView {
 
     public OutputView(Writer writer) {
         this.writer = writer;
+    }
+    
+    public static void displayProducts(Inventory inventory) {
+
+        System.out.println("안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.");
+        List<Product> products = inventory.getProducts();
+
+        for (Product product : products) {
+            String promoMessage = getPromotionMessage(product);
+            String regularStockMessage = getStockMessage(product.getRegularStock(), "재고 없음");
+            String promotionStockMessage = getStockMessage(product.getPromotionStock(), "");
+
+            printProductInfo(product, regularStockMessage, promotionStockMessage, promoMessage);
+        }
+        System.out.println();
+
+    }
+
+    private static String getPromotionMessage(Product product) {
+        if (product.getPromotion() != null && product.getPromotion().getType() != PromotionType.NONE) {
+            return product.getPromotion().getType().getDisplayName();
+        }
+        return "";
+    }
+
+    private static String getStockMessage(int stock, String noStockMessage) {
+        if (stock > 0) {
+            return stock + "개";
+        }
+        return noStockMessage;
+    }
+
+    private static void printProductInfo(Product product, String regularStockMessage, String promotionStockMessage,
+                                         String promoMessage) {
+        if (!promotionStockMessage.isEmpty()) {
+            System.out.printf("- %s %,.0f원 %s %s\n", product.getName(), product.getPrice(), promotionStockMessage,
+                    promoMessage);
+        }
+        if (product.getRegularStock() > 0 || promotionStockMessage.isEmpty()) {
+            System.out.printf("- %s %,.0f원 %s\n", product.getName(), product.getPrice(), regularStockMessage);
+        }
+
+    }
+
+    public static void displayFinalReceipt(Inventory inventory, Map<String, Map<String, Integer>> purchaseResults,
+                                           MembershipDiscount membershipDiscount, boolean hasMembershipDiscount) {
+        Map<String, Integer> purchaseDetails = purchaseResults.get("purchaseDetails");
+        Map<String, Integer> bonusDetails = purchaseResults.get("bonusDetails");
+
+        Receipt receipt = new Receipt(purchaseDetails, bonusDetails);
+        receipt.calculateCosts(inventory);
+        double membershipDiscountAmount = receipt.applyMembershipDiscount(membershipDiscount, hasMembershipDiscount);
+        receipt.displayReceipt(membershipDiscountAmount);
     }
 
 
