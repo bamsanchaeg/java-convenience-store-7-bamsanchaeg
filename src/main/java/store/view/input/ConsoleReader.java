08ed2219e58593f9499210f9966e3a6c7190883c
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2024-11-10 21:28:39 +0900

    feat(ConsoleReader) : ConsoleReader 기능 구현
    Reader 인터페이스 구현 후 상속

---
+++ src/main/java/store/view/input/ConsoleReader.java
@@ -0,0 +1,17 @@
+package store.view.input;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class ConsoleReader implements Reader {
+
+    @Override
+    public String read() {
+        return Console.readLine();
+    }
+
+    @Override
+    public void close(){
+        Console.close();
+    }
+
+}
---
+++ src/main/java/store/view/input/Reader.java
@@ -0,0 +1,7 @@
+package store.view.input;
+
+public interface Reader {
+    String read();
+
+    void close();
+}
