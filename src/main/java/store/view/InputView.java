77f63cc3be7fdb7d0794c601ad2f8f06ee0d2e4f
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2024-11-10 22:09:24 +0900

    feat(InputView) : 파일 입출력 파싱 기능 및 confirmUserInput 기능 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,5 +1,9 @@
 package store.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
 import store.view.input.ConsoleReader;
 import store.view.input.Reader;
 
@@ -13,7 +17,26 @@ public class InputView {
         this.reader = reader;
     }
 
+    public Map<String, Integer> purchaseProduct() {
+        System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
+        return parsePurchaseRequest(Console.readLine());
+    }
 
+
+    // 사용자 입력 확인 메서드
+    public static boolean confirmUserInput() {
+        String response = Console.readLine().trim().toUpperCase();
+        return "Y".equals(response);
+    }
+
+    private Map<String, Integer> parsePurchaseRequest(String purchaseRequest) {
+        return Arrays.stream(purchaseRequest.replaceAll("[\\[\\]]", "").split(","))
+                .map(item -> item.split("-"))
+                .collect(Collectors.toMap(
+                        parts -> parts[0],
+                        parts -> Integer.parseInt(parts[1])
+                ));
+    }
 
 
 }
